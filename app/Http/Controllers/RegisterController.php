34499adc9de81976e7ca7964ebf43f0356818343
muhtarudin <muhtarudin@javan.co.id>
2018-06-12 11:43:06 +0700

    Add validation error for register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Repositories\UserRepository;
+use App\Http\Requests\StoreUser;
 
 class RegisterController extends Controller
 {
@@ -19,7 +20,7 @@ class RegisterController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreUser $request)
     {
         //https://stackoverflow.com/questions/3825990/http-response-code-for-post-when-resource-already-exists
         $statusConflict = 409;
