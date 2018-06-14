ea675c196dae7c6d08753d8b2e38b71a6fa10719
muhtarudin <muhtarudin@javan.co.id>
2018-06-14 11:24:05 +0700

    Fix unauthorize for dummy user admin

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,7 +17,7 @@ class UsersTableSeeder extends Seeder
             [
                 'name' => 'admin',
                 'email' => 'admin@admin.com',
-                'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+                'password' => 'secret',
                 'remember_token' => str_random(10),
             ]
         );
